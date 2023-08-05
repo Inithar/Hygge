67d68b7fd6004b6b58094e40de4071705a60c3d6
Szymon Świtała <szymon.switala@proton.me>
2023-08-05 14:00:30 +0200

    feat: add Slider component

---
+++ src/components/Slider/Slider.styled.ts
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import { media } from "../../styles/helpers";
+
+export const Container = styled.div<{ gap?: string }>`
+  position: relative;
+
+  .slick-track {
+    & > * {
+      margin-right: ${({ gap }) => gap ?? 0};
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+
+  .slick-dots {
+    position: static;
+    display: flex !important;
+    justify-content: center;
+    gap: 1.6rem;
+    margin-top: 3.2rem;
+
+    ${media("sm")} {
+      margin-top: 4rem;
+    }
+
+    li {
+      margin: 0;
+      width: unset;
+      height: unset;
+
+      button {
+        width: 0.8rem;
+        height: 0.8rem;
+        padding: 0;
+        background-color: ${({ theme }) => theme.colors.primary};
+        border-radius: 50%;
+        outline: 2px solid transparent;
+        transition: background-color 0.3s, outline-color 0.3s;
+
+        &::before {
+          content: unset;
+        }
+
+        &:focus-visible {
+          background-color: ${({ theme }) => theme.colors.tertiary.two};
+          outline-color: ${({ theme }) => theme.colors.tertiary.two};
+        }
+
+        &:active {
+          background-color: transparent;
+          outline-color: ${({ theme }) => theme.colors.primary};
+        }
+      }
+    }
+
+    .slick-active button {
+      background-color: transparent;
+      outline-color: ${({ theme }) => theme.colors.primary};
+    }
+  }
+
+  .slider-controls {
+    position: absolute;
+    top: 22rem;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    gap: 1.6rem;
+    z-index: 100;
+
+    button {
+      background-color: transparent;
+      border: none;
+      border-radius: 50%;
+    }
+  }
+`;
---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,54 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { ReactNode, useRef, useState } from "react";
+import SlickSlider, { Settings } from "react-slick";
+
+import { Icon } from "../Icon/Icon";
+import { Container } from "./Slider.styled";
+
+type SliderProps = {
+  children: ReactNode;
+  settings: Settings;
+  gap?: string;
+};
+
+export const Slider = ({ settings, children, gap }: SliderProps) => {
+  const [sliderRef, setSliderRef] = useState<SlickSlider | null>(null);
+  const clickableRef = useRef(true);
+
+  const handleClick = (e: MouseEvent) => {
+    if (!clickableRef.current) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+
+    clickableRef.current = true;
+  };
+
+  const swipeEvent = () => {
+    if (sliderRef?.innerSlider?.list) {
+      sliderRef.innerSlider.list.onclick = handleClick;
+      clickableRef.current = false;
+    }
+  };
+
+  return (
+    <Container gap={gap}>
+      {settings.arrows && (
+        <div className="slider-controls">
+          <button onClick={sliderRef?.slickPrev} aria-label="Previous slide">
+            <Icon src="/icons/arrow-prev.svg" paddingSize="md" variation="fill" />
+          </button>
+          <button onClick={sliderRef?.slickNext} aria-label="Next slide">
+            <Icon src="/icons/arrow-next.svg" paddingSize="md" variation="fill" />
+          </button>
+        </div>
+      )}
+
+      <SlickSlider ref={setSliderRef} {...settings} arrows={false} swipeEvent={swipeEvent}>
+        {children}
+      </SlickSlider>
+    </Container>
+  );
+};
