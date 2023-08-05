3f7ab47c1a233e2c44361dcecdaa9c83186b916f
Szymon Świtała <szymon.switala@proton.me>
2023-08-05 14:05:52 +0200

    feat: add Category component

---
+++ src/screens/Home/Category/Category.styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import { rgba } from "../../../styles/helpers";
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.6rem;
+  padding: 2.6rem 1rem;
+  width: 12.8rem;
+  background-color: ${({ theme }) => theme.colors.accent.one};
+  border: 2px solid transparent;
+  border-radius: ${({ theme }) => theme.borderRadius.sm};
+  transition: background-color 0.3s, border-color 0.5s;
+
+  &:hover,
+  &:focus-visible {
+    outline: none;
+    background-color: ${({ theme }) => rgba(theme.colors.accent.one, 0.24)};
+    border-color: ${({ theme }) => theme.colors.primary};
+  }
+`;
---
+++ src/screens/Home/Category/Category.tsx
@@ -0,0 +1,18 @@
+import { Icon } from "../../../components/Icon/Icon";
+import { Text } from "../../../components/Text";
+import { StyledLink } from "./Category.styled";
+
+type CategoryProps = {
+  icon: string | null;
+  name: string | null;
+};
+
+export const Category = ({ icon, name }: CategoryProps) => (
+  // TODO add the correct path
+  <StyledLink to={"/products"} draggable="false">
+    <Icon src={icon ?? ""} alt={`${name} icon`} iconSize="lg" />
+    <Text fontSize="sm" fontFamily={"montserratSemiBold"}>
+      {name}
+    </Text>
+  </StyledLink>
+);
