8efc4f903466edb86a4b7bd2f3306492668d738f
Szymon Świtała <szymon.switala@proton.me>
2023-08-09 16:56:49 +0200

    feat: add ProductItem component

---
+++ src/components/ProductItem/ProductItem.styled.ts
@@ -0,0 +1,128 @@
+import styled, { css } from "styled-components";
+
+import { media, rgba } from "../../styles/helpers";
+
+export const StyledProductItem = styled.div`
+  position: relative;
+`;
+
+export const Container = styled.div`
+  position: relative;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 3.2rem;
+    width: 100%;
+    height: 31.2rem;
+    background-color: ${({ theme }) => theme.colors.accent.one};
+    border-radius: ${({ theme }) => theme.borderRadius.lg};
+  }
+
+  ${media("md")} {
+    a {
+      height: 27.2rem;
+    }
+
+    img {
+      transform: scale(0.8);
+    }
+
+    &:hover > button {
+      opacity: 1;
+    }
+  }
+`;
+
+export const AddToCartBtn = styled.button`
+  display: none;
+
+  ${media("md")} {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: inline-block;
+    padding: 1.2rem 2.4rem;
+    min-width: 15rem;
+    border: none;
+    outline: 2px solid transparent;
+    outline-offset: 3px;
+    opacity: 0;
+    transition: outline-color 0.3s, opacity 0.3s;
+
+    ${({ theme }) => css`
+      font-size: ${theme.fontSizes.sm};
+      font-family: ${theme.fontFamilies.montserrat.semiBold};
+      color: ${theme.colors.basic.white};
+      background-color: ${theme.colors.primary};
+      border-radius: ${theme.borderRadius.md};
+    `}
+
+    &:hover, &:focus {
+      outline-color: ${({ theme }) => theme.colors.primary};
+    }
+
+    &:focus-visible {
+      opacity: 1;
+    }
+  }
+`;
+
+export const ProductData = styled.div`
+  margin-top: 3.2rem;
+  font-family: ${({ theme }) => theme.fontFamilies.montserrat.semiBold};
+`;
+
+export const Name = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes["2xl"]};
+`;
+
+export const Details = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+  margin-top: 1.6rem;
+`;
+
+export const Price = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+export const Old = styled.p<{ isDisplay: boolean }>`
+  ${({ theme, isDisplay }) => css`
+    display: ${isDisplay ? "block" : "none"};
+    font-size: ${theme.fontSizes.sm};
+    color: ${rgba(theme.colors.text.dark, 0.24)};
+  `}
+
+  text-decoration: line-through;
+`;
+
+export const Current = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+`;
+
+export const BadgeContainer = styled.div`
+  position: absolute;
+  top: 2.4rem;
+  right: -2.4rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  z-index: 10;
+`;
+
+export const DetailsSkeleton = styled.div`
+  margin-top: 3.2rem;
+
+  & > div:nth-child(2) {
+    display: flex;
+    align-items: center;
+    gap: 2.4rem;
+    margin-top: 1.6rem;
+  }
+`;
---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,86 @@
+import { Link } from "react-router-dom";
+
+import { Badge } from "../Badge/Badge";
+import { BadgeColor } from "../Badge/Badge.styled";
+import { Skeleton } from "../Skeleton";
+import {
+  Container,
+  AddToCartBtn,
+  StyledProductItem,
+  ProductData,
+  Name,
+  Details,
+  Price,
+  Old,
+  Current,
+  BadgeContainer,
+  DetailsSkeleton,
+} from "./ProductItem.styled";
+
+type ProductProps = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  sale: number | null;
+  isNew: boolean;
+  category: {
+    name: string;
+    color: string;
+  };
+};
+
+export const ProductItem = ({ id, name, image, price, sale, category, isNew }: ProductProps) => {
+  const currentPrice = sale ? Math.floor(((100 - sale) / 100) * price) : price;
+
+  return (
+    <StyledProductItem>
+      <BadgeContainer>
+        {isNew && (
+          <Badge fill color="green">
+            NEW IN
+          </Badge>
+        )}
+
+        {sale && <Badge fill color="red">{`${sale.toString()}% OFF`}</Badge>}
+      </BadgeContainer>
+
+      <Container>
+        <Link to={`/products/${id}`} aria-label={`Link to ${name} product details`}>
+          <img src={image} alt={`Picture of ${name} product`} />
+        </Link>
+
+        <AddToCartBtn aria-label={`Add ${name} to cart`}>Add to Cart</AddToCartBtn>
+      </Container>
+
+      <ProductData>
+        <Link to={`/products/${id}`}>
+          <Name>{name}</Name>
+        </Link>
+
+        <Details>
+          <Badge color={category.color as BadgeColor}>{category.name}</Badge>
+          <Price>
+            <Old isDisplay={Boolean(sale)}>${price}</Old>
+            <Current>${currentPrice}</Current>
+          </Price>
+        </Details>
+      </ProductData>
+    </StyledProductItem>
+  );
+};
+
+export const ProductItemSkeleton = () => (
+  <div>
+    <Skeleton width={"100%"} height={"31.2rem"} borderRadius="lg" />
+
+    <DetailsSkeleton>
+      <Skeleton width="55%" height="3.2rem" />
+
+      <div>
+        <Skeleton width="40%" height="3.7rem" borderRadius="sm" />
+        <Skeleton width="18%" height="2.4rem" />
+      </div>
+    </DetailsSkeleton>
+  </div>
+);
