d681e830e88115aad6745a0463cfc86a729643d2
Szymon Świtała <szymon.switala@proton.me>
2023-08-16 17:06:18 +0200

    feat: add created components to home screen

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,17 @@
-import { Hero } from "./components/Hero/Hero";
+import { Hero } from "./Hero/Hero";
+import { Categories } from "./Categories/Categories";
+import { Products } from "./Products/Products";
+import { WhyUs } from "./WhyUs/WhyUs";
+import { Reviews } from "../../components/Reviews/Reviews";
+
+import { reviews } from "../../data/home";
 
 export const Home = () => (
   <>
     <Hero />
+    <Categories />
+    <Products />
+    <WhyUs />
+    <Reviews reviews={reviews} />
   </>
 );
