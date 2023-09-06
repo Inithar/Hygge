32dea4d6b8aa7e942352dc23e76933aaf0a07aed
Inithar <szymon.switala@proton.me>
2023-09-06 13:52:25 +0200

    feat: add Modal component

---
+++ src/components/Modal/Modal.styled.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { media, rgba } from "../../styles/helpers";
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => rgba(theme.colors.basic.black, 0.3)};
+  backdrop-filter: blur(4px);
+  z-index: 1000;
+`;
+
+export const StyledModal = styled.div<{ maxWidth?: string }>`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2.6rem 2.8rem;
+  width: calc(100% - 2 * 2.4rem);
+  max-width: ${({ maxWidth }) => maxWidth ?? "60rem"};
+  background-color: ${({ theme }) => theme.colors.basic.white};
+  border-radius: ${({ theme }) => theme.borderRadius.md};
+  box-shadow: ${({ theme }) => theme.shadows.lg};
+
+  ${media("xs")} {
+    padding: 3.2rem 4rem;
+  }
+
+  ${media("sm")} {
+    width: calc(100% - 2 * 4rem);
+  }
+`;
+
+export const ModalHeader = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 1.6rem;
+  font-family: ${({ theme }) => theme.fontFamilies.montserrat.semiBold};
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+
+  & > button {
+    display: grid;
+    place-items: center;
+    background-color: transparent;
+    border: none;
+  }
+
+  ${media("xs")} {
+    font-size: ${({ theme }) => theme.fontSizes.xl};
+
+    & > button > img {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+`;
+
+export const Divider = styled.div`
+  margin-block: 2.4rem;
+  height: 0.1rem;
+  width: 100%;
+  background-color: ${({ theme }) => rgba(theme.colors.basic.black, 0.1)};
+
+  ${media("xs")} {
+    margin-block: 2.4rem;
+  }
+`;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,120 @@
+import { createPortal } from "react-dom";
+import {
+  ReactElement,
+  ReactNode,
+  cloneElement,
+  createContext,
+  useContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react";
+
+import { useOutsideClick } from "../../hooks/useOutsideClick";
+import { useDisableBodyScroll } from "../../hooks/useDisableBodyScroll";
+
+import { FocusTrap } from "../FocusTrap";
+import { Overlay, StyledModal, ModalHeader, Divider } from "./Modal.styled";
+
+type OnWindowCloseType = (() => void) | undefined;
+
+type ModalContextType = {
+  openName: string;
+  open: (windowName: string) => void;
+  close: () => void;
+  onWindowClose: Dispatch<SetStateAction<OnWindowCloseType>>;
+};
+
+type OpenProps = {
+  children: ReactElement;
+  opens: string;
+};
+
+type WindowProps = {
+  children: ReactNode;
+  name: string;
+  header: string;
+  maxWidth?: string;
+  onClose?: () => void;
+};
+
+export const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const Modal = ({ children }: { children: ReactNode }) => {
+  const [openName, setOpenName] = useState("");
+  const [onWindowClose, setOnWindowClose] = useState<OnWindowCloseType>(undefined);
+
+  function open(windowName: string) {
+    setOpenName(windowName);
+  }
+
+  function close() {
+    setOpenName("");
+    onWindowClose?.();
+  }
+
+  return (
+    <ModalContext.Provider value={{ openName, open, close, onWindowClose: setOnWindowClose }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+const Open = ({ children, opens: opensWindowName }: OpenProps) => {
+  const context = useContext(ModalContext);
+
+  if (context === undefined) {
+    throw new Error("Modal.Open was used outside of the modal provider");
+  }
+
+  const { open } = context;
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+};
+
+const Window = ({ children, name, maxWidth, header, onClose }: WindowProps) => {
+  const context = useContext(ModalContext);
+
+  if (context === undefined) {
+    throw new Error("Modal.Window was used outside of the modal provider");
+  }
+
+  const { close, onWindowClose, openName } = context;
+
+  const isWindowOpen = name === openName;
+  useDisableBodyScroll(isWindowOpen);
+
+  const ref = useOutsideClick<HTMLDivElement>(close);
+
+  useEffect(() => {
+    if (onClose && isWindowOpen) {
+      onWindowClose(() => onClose);
+    }
+  }, [isWindowOpen, onWindowClose, onClose]);
+
+  return isWindowOpen
+    ? createPortal(
+        <FocusTrap active={isWindowOpen}>
+          <Overlay>
+            <StyledModal ref={ref} maxWidth={maxWidth}>
+              <ModalHeader>
+                <p>{header}</p>
+                <button type="button" onClick={close} aria-label="Close modal">
+                  <img src="/icons/delete.svg" alt="close modal icon" />
+                </button>
+              </ModalHeader>
+
+              <Divider />
+
+              <div>{children}</div>
+            </StyledModal>
+          </Overlay>
+        </FocusTrap>,
+        document.body
+      )
+    : null;
+};
+
+Modal.Open = Open;
+Modal.Window = Window;
