9757d561a310df73dc2c6abb4e3352c11e9b8a5c
Inithar <szymon.switala@proton.me>
2023-09-09 11:47:37 +0200

    fix: add missing useLogout hook

---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { logout } from "../services/authApi";
+
+export const useLogout = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading, isError } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.removeQueries();
+      navigate("/login", { replace: true });
+    },
+  });
+
+  return { logout: mutate, isLoading, isError };
+};
