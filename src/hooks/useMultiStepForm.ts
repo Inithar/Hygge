022304ab4d0e261099eb85ce1f86bfe26312bcfa
Inithar <szymon.switala@proton.me>
2023-09-16 12:42:59 +0200

    feat: add useMultiStepForm hook

---
+++ src/hooks/useMultiStepForm.ts
@@ -0,0 +1,39 @@
+import { ReactElement, useState } from "react";
+
+export const useMultiStepForm = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((prev) => {
+      if (prev > steps.length) {
+        return prev;
+      }
+
+      return prev + 1;
+    });
+  }
+
+  function back() {
+    setCurrentStepIndex((prev) => {
+      if (prev <= 0) {
+        return prev;
+      }
+
+      return prev - 1;
+    });
+  }
+
+  function goTo(index: number) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    step: steps[currentStepIndex],
+    currentStepIndex,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
+    goTo,
+    next,
+    back,
+  };
+};
