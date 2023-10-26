845b0c62a3c0ede1f2c04094326d5b6ca52efae2
Inithar <szymon.switala@proton.me>
2023-10-26 13:19:57 +0200

    fix: fix the problem with jumping modal

--- src/hooks/useDisableBodyScroll.ts
+++ src/hooks/useDisableBodyScroll.ts
@@ -3,5 +3,6 @@ import { useEffect } from "react";
 export const useDisableBodyScroll = (isHidden: boolean) => {
   useEffect(() => {
     document.body.style.overflow = isHidden ? "hidden" : "visible";
+    document.body.style.paddingRight = isHidden ? "1.7rem" : "0rem";
   }, [isHidden]);
 };
