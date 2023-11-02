9c17e20e4865d1daf713fe7ff58f630993f7fefe
Inithar <szymon.switala@proton.me>
2023-11-02 11:57:59 +0100

    fix: remove unused files

--- src/services/productsApi.ts
+++ src/services/productsApi.ts
@@ -4,7 +4,7 @@ import { PopulateProduct } from "../types/collection";
 export const getProducts = async () => {
   const { data: products, error } = await supabase
     .from("products")
-    .select(`*, category("name", "color"), brand("name")`)
+    .select(`*, category("name", "color")`)
     .returns<PopulateProduct[]>();
 
   if (error) {
