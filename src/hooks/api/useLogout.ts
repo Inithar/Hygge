22c16462bfed2d3bea321ccfa2bfeb607f6afbbd
Inithar <szymon.switala@proton.me>
2024-01-03 12:54:43 +0100

    feat: cart is cleared after logging out

--- src/hooks/api/useLogout.ts
+++ src/hooks/api/useLogout.ts
@@ -1,16 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { useCart } from "../context/useCart";
 import { logout } from "../../services/authApi";
 
 export const useLogout = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
+  const { clearCart } = useCart();
+
   const { mutate, isLoading, isError } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
       queryClient.removeQueries();
+      clearCart();
       navigate("/login", { replace: true });
     },
   });
