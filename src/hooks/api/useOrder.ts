4449ea3c761304e668e17651dd3a2b941ad425dc
Inithar <szymon.switala@proton.me>
2024-01-05 16:01:55 +0100

    feat: add prefeatching

--- src/hooks/api/useOrder.ts
+++ src/hooks/api/useOrder.ts
@@ -1,11 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
 import { getOrder } from "../../services/ordersApi";
+import { getOrderedProducts } from "../../services/orderedProductsApi";
 
 export const useOrder = (id: number) => {
+  const queryClient = useQueryClient();
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["order", id],
     queryFn: () => getOrder(id),
   });
+
+  queryClient.prefetchQuery({ queryKey: ["orderedProducts", id], queryFn: () => getOrderedProducts(id) });
 
   return { order: data, isLoading, error };
 };
--- src/hooks/api/useOrders.ts
+++ src/hooks/api/useOrders.ts
@@ -1,11 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 
 import { getOrders } from "../../services/ordersApi";
 
 import { subtractDays } from "../../utils/subtractDays";
+import { ORDERS_PAGE_SIZE } from "../../constants/settings";
 
 export const useOrders = (customer?: string) => {
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
 
   const period = searchParams.get("period");
@@ -15,12 +18,30 @@ export const useOrders = (customer?: string) => {
       ? null
       : { field: "created_at", value: subtractDays(new Date(), Number(period)).toDateString() };
 
+  console.log(filter);
+
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["orders", filter, page, customer],
     queryFn: () => getOrders({ filter, page, customer }),
   });
+
+  const pageCount = Math.ceil(data?.count || 1 / ORDERS_PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["orders", filter, page + 1, customer],
+      queryFn: () => getOrders({ filter, page: page + 1, customer }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["orders", filter, page - 1, customer],
+      queryFn: () => getOrders({ filter, page: page - 1, customer }),
+    });
+  }
 
   return { orders: data?.orders, count: data?.count, isLoading, error };
 };
